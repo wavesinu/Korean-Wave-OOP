8912258a796fcdefaaa1b5a10db5eaf95d2386af
wavesinu <wavesinu@gmail.com>
2022-12-12 00:23:48 +0900

    update: update Main.java

--- src/Main.java
+++ src/Main.java
@@ -26,12 +26,12 @@ public class Main {
                     KpopFileControl kpopFileControl = new KpopFileControl();
                     System.out.println("K-pop에 관한 정보를 알고싶으시군요?");
                     System.out.println("원하시는 정보를 선택해주세요!");
-                    System.out.println("1. 대표적인 K-pop 아티스트 정보    | 2. 해당 아티스트들의 대표곡    | 3. K-pop 빌보드 차트 기록    | 4. 메인메뉴로 돌아가기");
+                    System.out.println("1. 대표적인 K-pop 아티스트 정보    | 2. 해당 아티스트들의 대표곡    | 3. K-pop 빌보드 차트 기록    | 4. 전체 정보 확인하기");
                     String infoChoice = sc.nextLine();
 
                     switch (infoChoice) {
                         case "1" -> {
-                            kpopFileControl.readName();
+                            kpopFileControl.readGroupName();
                         }
                         case "2" -> {
                             kpopFileControl.readSongs();
@@ -39,7 +39,9 @@ public class Main {
                         case "3" -> {
                             kpopFileControl.readBillboard();
                         }
-                        case "4" -> System.out.println("메인메뉴로 돌아갑니다.\n");
+                        case "4" -> {
+                            kpopFileControl.readAll();
+                        }
 
                     }
                 }
@@ -47,21 +49,19 @@ public class Main {
                     KdramaFileControl kdramaFileControl = new KdramaFileControl();
                     System.out.println("K-Drama에 관한 정보를 선택하셨습니다.");
                     System.out.println("원하시는 정보를 선택해주세요!");
-                    System.out.println("1. 대표적인 K-Drama    |    2. K-Drama 관련 해외 수상 내역    | 4. 메인메뉴로 돌아가기");
+                    System.out.println("1. 대표적인 K-Drama 작품    |    2. 해당 드라마 상세 정보    | 3. 메인메뉴로 돌아가기");
                     String infoChoice = sc.nextLine();
 
                     switch (infoChoice) {
-                        case "1":
-                            System.out.println("해외에서 인기 있는 대표적인 K-Drama는 다음과 같습니다.");
-                            kdramaFileControl.readAll();
-                            break;
-                        case "2":
-                            System.out.println("K-Drama 관련 해외 수상 내역은 다음과 같습니다.");
-                            // K-Drama 관련 해외 수상 내역 출력
-                            break;
-                        case "3":
-                            System.out.println("메인메뉴로 돌아갑니다.\n");
-                            break;
+                        case "1" -> {
+                            kdramaFileControl.readDramaName();
+                        }
+                        case "2" -> {
+                            kdramaFileControl.readDetailInfo();
+                        }
+                        case "3" -> {
+                            System.out.println("메인메뉴로 돌아갑니다.");
+                        }
                     }
 
 
@@ -70,17 +70,19 @@ public class Main {
                     KmoviesFileControl kmoviesFileControl = new KmoviesFileControl();
                     System.out.println("K-Movie에 관한 정보를 선택하셨습니다.");
                     System.out.println("원하시는 정보를 선택해주세요!");
-                    System.out.println("1. K-Movie 배우 정보    | 2. K-Movie 영화 정보    | 3. K-Movie 관련 해외 수상 내역    | 4. 메인메뉴로 돌아가기");
+                    System.out.println("1. 대표적인 K-Movie 작품    | 2. 해당 영화 상세 정보    |    3. 메인메뉴로 돌아가기");
                     String infoChoice = sc.nextLine();
 
                     switch (infoChoice) {
                         case "1" -> {
-                            System.out.println("해외에서 인기 있는 대표적인 K-Movie 배우는 다음과 같습니다.");
-                            kmoviesFileControl.readAll();
+                            kmoviesFileControl.readMoviesInfo();
                         }
-                        case "2" -> System.out.println("해당 배우들의 인기 영화 정보는 다음과 같습니다.");
-                        case "3" -> System.out.println("K-Movie 관련 해외 수상 내역은 다음과 같습니다.");
-                        case "4" -> System.out.println("메인메뉴로 돌아갑니다.\n");
+                        case "2" -> {
+                            kmoviesFileControl.readDetailInfo();
+                        }
+                        case "3" -> {
+                            System.out.println("메인메뉴로 돌아갑니다.");
+                        }
                     }
 
                 }
