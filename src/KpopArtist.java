7df2b81a47d846e56f01ff99c401b4a56f597053
wavesinu <wavesinu@gmail.com>
2022-12-12 00:22:41 +0900

    delete: package refactor

--- src/KpopArtist.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-
-public class KpopArtist extends Kpop{
-
-    KpopArtist(String groupName, String agency, List<String> memberList) {
-        super(groupName, agency, memberList);
-    }
-}
